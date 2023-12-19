cc16cc7130143904d6ded02c4f5055056d1576a5
PanhaLtd <panhaltb@gmail.com>
2023-12-19 17:36:47 +0700

    add sql

--- db_script.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE student 
-(
-	id          BIGINT      NOT NULL    PRIMARY KEY,
-	name        VARCHAR(32) NOT NULL,
-	dob         DATE        NOT NULL,
-	image_path VARCHAR(64)
-);
--- student.sql
+++ student.sql
@@ -3,5 +3,5 @@ CREATE TABLE student
 	id          BIGINT      NOT NULL    PRIMARY KEY,
 	name        VARCHAR(32) NOT NULL,
 	dob         DATE        NOT NULL,
-	image_path VARCHAR(64)
+	image_path  VARCHAR(64)
 );
